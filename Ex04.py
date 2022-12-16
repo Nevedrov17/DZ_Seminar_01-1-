02deaafe39176a0b5f9ecb2e91a185b81253e2d6
NEVA <neva.vka@yandex.ru>
2022-12-17 02:09:27 +0300

    Зaдание 4

--- Ex04.py
+++ Ex04.py
@@ -9,11 +9,14 @@ for i in range(n - 1, -1 * n):
     my_list.append(i + 1)
 print(my_list)
 
-poz1, poz2 = int(input('Введите первую позицию: ')), int(input('Введите вторую позицию: '))
-if poz1 > len(my_list)-1 or poz2 > len(my_list)-1:
-    print('Указанных позиций нет')
-else:
-    pr = my_list[poz1] * my_list[poz2]
-    print('Произведение указанных значений =', pr)
+poz1 = int(input('Введите первую позицию: '))
+while poz1 > len(my_list) - 1:
+    poz1 = int(input('Такой позиции нет, введите еще раз: '))
+poz2 = int(input('Введите вторую позицию: '))
+while poz2 > len(my_list) - 1:
+    poz2 = int(input('Такой позиции нет, введите еще раз: '))
+pr = my_list[poz1] * my_list[poz2]
+print('Произведение указанных значений =', pr)
+
 
 
